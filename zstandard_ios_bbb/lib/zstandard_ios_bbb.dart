0d2b52fd1b06724d2bda611aca6a181e02d7e5c3
Efra Espada <efraespada@gmail.com>
2024-10-25 04:03:58 +0200

    feature: removed test

--- zstandard_ios_bbb/lib/zstandard_ios_bbb.dart
+++
@@ -1,8 +0,0 @@
-
-import 'zstandard_ios_bbb_platform_interface.dart';
-
-class ZstandardIosBbb {
-  Future<String?> getPlatformVersion() {
-    return ZstandardIosBbbPlatform.instance.getPlatformVersion();
-  }
-}
