d18efc7eeb901b6d02345a2f87e6f31b4bcc9c55
Efra Espada <efraespada@gmail.com>
2024-11-03 17:18:01 +0100

    feature: testing windows

--- zstandard_cli/lib/src/utils/lib_loader.dart
+++ zstandard_cli/lib/src/utils/lib_loader.dart
@@ -5,8 +5,9 @@ import 'package:path/path.dart' as path;
 
 DynamicLibrary openZstdLibrary() {
   if (Platform.isWindows) {
-    final libPath = path.join(
-        Directory.current.path, 'lib', 'src', 'bin', 'zstandard_windows.dll');
+    final String arch = Platform.version.contains('ARM64') ? "arm64" : "x64";
+    final libPath = path.join(Directory.current.path, 'lib', 'src', 'bin',
+        'zstandard_windows_$arch.dll');
     return DynamicLibrary.open(libPath);
   } else if (Platform.isMacOS) {
     final libPath = path.join(Directory.current.path, 'lib', 'src', 'bin',
