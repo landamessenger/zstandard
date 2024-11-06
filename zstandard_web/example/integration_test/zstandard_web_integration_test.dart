b487287e97705d6a83addac1ac4b1a50fc239aa3
Efra Espada <efraespada@gmail.com>
2024-11-06 20:38:10 +0100

    feature: updated version and workflow

--- zstandard_web/example/integration_test/zstandard_web_integration_test.dart
+++ zstandard_web/example/integration_test/zstandard_web_integration_test.dart
@@ -4,6 +4,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:zstandard_platform_web_example/main.dart';
 import 'package:zstandard_web/zstandard_web.dart';
+// ignore: avoid_web_libraries_in_flutter
 import 'dart:js' as js;
 
 void main() {
