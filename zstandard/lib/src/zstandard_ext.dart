4655c1556bb466cc1f74ac24126d1a7726026da3
Efra Espada <efraespada@gmail.com>
2024-11-07 22:48:35 +0100

    feature: updated version and workflows

--- zstandard/lib/src/zstandard_ext.dart
+++ zstandard/lib/src/zstandard_ext.dart
@@ -3,10 +3,10 @@ import 'dart:typed_data';
 import 'package:zstandard/zstandard.dart';
 
 extension ZstandardExt on Uint8List? {
-  Future<Uint8List?> compress() async {
+  Future<Uint8List?> compress({int compressionLevel = 3}) async {
     var data = this;
     if (data == null) return null;
-    return Zstandard().compress(data);
+    return Zstandard().compress(data, compressionLevel);
   }
 
   Future<Uint8List?> decompress() async {
